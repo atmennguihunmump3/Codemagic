e4fe3cec5fa52b392ab37c46ccadf0a1f2f8df4f
atmennguihunmump3 <atmen.nguihunmum.p3@gmail.com>
2026-08-21 13:03:31 +0800

    Delete Dola app directory

--- Dola app/lib/core/utils/permission_helper.dart
+++
@@ -1,7 +0,0 @@
-enum AccessLevel { viewer, executer }
-
-class PermissionHelper {
-  static bool canAdd(AccessLevel level) => level == AccessLevel.executer;
-  static bool canDelete(AccessLevel level) => level == AccessLevel.executer;
-  static bool canEdit(AccessLevel level) => level == AccessLevel.executer;
-}
